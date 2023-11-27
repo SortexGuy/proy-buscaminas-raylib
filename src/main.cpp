695835bcf1d40bd54560a1026ae3e18649e57ca1
Rubén A <sortexjuegos@gmail.com>
2023-11-27 11:47:10 -0400

    Initial Commit yesyes

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <thread>
+#include "raylib.h"
+
+int main(int argc, char* argv[]) {
+  InitWindow(960, 540, "Buscaminas Estructurado");
+  SetTargetFPS(60);
+  WaitTime(2);
+  CloseWindow();
+  return 0;
+}
